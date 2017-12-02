e47f779af29e32459dc5fe6df04f25f5d6e9173f
Mike <demurtasmichele@gmail.com>
2017-12-02 19:31:37 +0100

    helpBot

---
+++ Dialogs/DefaultDialog.cs
@@ -0,0 +1,13 @@
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using System;
+
+namespace LUISBotApplication.Dialogs
+{
+	[[LuisModel(“2d653e62-8442-4494-bf8c-9c96dfdd4ef7”, “46aa1a0ea2114db1838cf6652754fc7b”)]
+	[Serializable]
+
+	public class DefaultDialog : LuisDialog<object>
+	{
+	}
+}
